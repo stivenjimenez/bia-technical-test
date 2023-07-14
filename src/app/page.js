4f5f2d9ce104ac235db7055d58e5ad55cde90785
stiven jimenez <sajqprogramming@gmail.com>
2023-07-14 16:01:11 -0500

    ♻️  delete child component and add the content to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import Countries from "@/components/Countries";
+import CountryCard from "@/components/ContryCard";
 
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch(
+        "https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags"
+      );
+      const data = await response.json();
+      setCountries(data);
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
     <main className={styles.main}>
-      <Countries />
+      <div className={styles.grid}>
+        {countries.map((country) => (
+          <CountryCard key={country.name.common} country={country} />
+        ))}
+      </div>
     </main>
   );
 }
