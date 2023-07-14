323802cf927b2aa6175cd94d22b9a0bebbf80d31
stiven jimenez <sajqprogramming@gmail.com>
2023-07-14 15:53:06 -0500

    ✨ add detail  of country

---
+++ src/app/country/[countryName]/page.js
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./country.module.css";
+async function getCountry(name) {
+  const res = await fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags `
+  );
+  const country = await res.json();
+  return country;
+}
+
+export default async function Country({ params }) {
+  const data = await getCountry(params.countryName);
+  const country = data[0];
+
+  const nativeName = Object.entries(country.name.nativeName).map(
+    (name) => name[1].common
+  )[0];
+
+  const currencies = Object.entries(country.currencies).map(
+    (currency) => currency[1].name
+  )[0];
+
+  const languages = Object.values(country.languages).join(", ");
+
+  return (
+    <main className={styles.main}>
+      <Link className={styles.link} href="/">
+        Back
+      </Link>
+
+      <section className={styles.wrapper}>
+        <Image
+          unoptimized
+          width={560}
+          height={400}
+          src={country.flags.svg}
+          alt={country.name.common}
+          className={styles.flag}
+        />
+
+        <div className={styles.info}>
+          <h2>{country.name.common}</h2>
+          <div className={styles.infoColumns}>
+            <div>
+              <p className={styles.infoItem}>
+                <span>Native Name: </span>
+                {nativeName}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Population: </span>
+                {country.population}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Region: </span>
+                {country.region}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Sub Region: </span>
+                {country.subregion}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Capital: </span>
+                {country.capital[0]}
+              </p>
+            </div>
+            <div>
+              <p className={styles.infoItem}>
+                <span>Top Level Domain: </span>
+                {country.tld[0]}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Currencies: </span>
+                {currencies}
+              </p>
+              <p className={styles.infoItem}>
+                <span>Languages: </span>
+                {languages}
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <p className={styles.infoItem}>
+              <span>Border Countries: </span>
+            </p>
+
+            {country.borders.map((border) => {
+              return (
+                <span className={styles.border} key={border}>
+                  {border}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
--- src/components/ContryCard/index.js
+++ src/components/ContryCard/index.js
@@ -1,13 +1,20 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import styles from "./countryCard.module.css";
 export default function CountryCard({ country }) {
+  const router = useRouter();
+
   return (
-    <div className={styles.card}>
+    <div
+      className={styles.card}
+      onClick={() => router.push(`/country/${country.name.common}`)}
+    >
       <Image
         unoptimized
         width={267}
         height={160}
-        src={country.flags.png}
+        src={country.flags.svg}
         alt={country.name.common}
         className={styles.flag}
       />
