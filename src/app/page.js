a3b12530c9a998fc05dc36168506b6e1703caf11
stiven jimenez <sajqprogramming@gmail.com>
2023-07-14 16:55:08 -0500

    ✨  adde feature to filter countries by name

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,29 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import CountryCard from "@/components/ContryCard";
+import { BiSearch } from "react-icons/bi";
 
 export default function Home() {
   const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    const filterCountries = (searchText) => {
+      if (searchText.trim() === "") {
+        setFilteredCountries(countries);
+      } else {
+        const filtered = countries.filter((country) => {
+          const countryName = country.name.common.toLowerCase();
+          return countryName.includes(searchText.toLowerCase());
+        });
+        setFilteredCountries(filtered);
+      }
+    };
+
+    filterCountries(filterText);
+  }, [filterText, countries]);
+
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await fetch(
@@ -19,8 +39,18 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      <div className={styles.search}>
+        <BiSearch className={styles.searchIcon} size={18} color="#848484" />
+        <input
+          type="text"
+          placeholder="Search for a country..."
+          onChange={(e) => setFilterText(e.target.value)}
+          value={filterText}
+          className={styles.searchInput}
+        />
+      </div>
       <div className={styles.grid}>
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <CountryCard key={country.name.common} country={country} />
         ))}
       </div>
